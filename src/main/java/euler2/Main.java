e6b7a626a27da02ef999953df09fa519b332b6d5
ToFatToBat <kpapiernia@wp.pl>
2020-08-09 22:39:59 +0200

    Poprawka na BigInteger - oczyszczenie z syfu

--- src/main/java/euler2/Main.java
+++ src/main/java/euler2/Main.java
@@ -11,22 +11,18 @@ public class Main {
     public static void main(String[] args) {
 
 
-       BigInteger fibonacciSum =
+        BigInteger fibonacciSum =
 
-            Stream.iterate(new BigInteger[]{BigInteger.ZERO, BigInteger.ONE}, f -> new BigInteger[]{f[1], f[0].add(f[1])})
-                    .limit(600)
-                    .map(t -> t[0])
-                    .filter(Main::isLessThat4Mln)
-                    .filter(Main::isEven)
-                    .reduce(BigInteger.ZERO, BigInteger::add);
-                   // .sum();
-            // .forEach(System.out::println);
-            //   System.out.println(fibonacciSum);
+                Stream.iterate(new BigInteger[]{BigInteger.ZERO, BigInteger.ONE}, f -> new BigInteger[]{f[1], f[0].add(f[1])})
+                        .limit(600)
+                        .map(t -> t[0])
+                        .filter(Main::isLessThat4Mln)
+                        .filter(Main::isEven)
+                        .reduce(BigInteger.ZERO, BigInteger::add);
 
+        System.out.println(fibonacciSum);
 
-            int result = 1134903170 + 1836311903;
-            System.out.println(result);
-        }
+    }
 
     private static boolean isLessThat4Mln(BigInteger number) {
         return number.compareTo(BigInteger.valueOf(4000000)) < 0;
@@ -35,6 +31,6 @@ public class Main {
     private static boolean isEven(BigInteger number) {
         return number.mod(BigInteger.valueOf(2)).equals(BigInteger.ZERO);
     }
-    }
+}
 
 
