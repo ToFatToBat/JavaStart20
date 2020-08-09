a7a686a601f728e7a8ea2686de2ed0b5af549402
ToFatToBat <kpapiernia@wp.pl>
2020-08-09 17:11:48 +0200

    Poprawka

--- src/main/java/euler2/Main.java
+++ src/main/java/euler2/Main.java
@@ -13,7 +13,7 @@ public class Main {
 
         int fibonacciSum =
                 Stream.iterate(new int[]{0, 1}, f -> new int[]{f[1], f[0] + f[1]})
-                        .limit(500)
+                        .limit(600)
                         .map(t -> t[0])
                         .filter(numLessThenFourMil -> numLessThenFourMil < 4000000)
                         .filter(evenNumbers -> evenNumbers % 2 == 0)
